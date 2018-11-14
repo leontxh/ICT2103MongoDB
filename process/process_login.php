76e060bf99b3f8794784d2219255f080bbe8990d
QJunHao <jun_hao995@hotmail.com>
2018-11-14 13:20:32 +0800

    Add files via upload

--- process/process_login.php
+++ process/process_login.php
@@ -17,29 +17,26 @@
                     if(isset($_POST['login']))
                     {
                         // Escape user inputs for security
-                        $loginusername = mysqli_real_escape_string($conn, $_REQUEST['loginusername']);
-                        $loginpassword = mysqli_real_escape_string($conn, md5($_REQUEST['loginpassword']));
-                            
-                        // attempt insert query execution
-                        $sql = "SELECT userID, username, password FROM user where username='$loginusername' AND password='$loginpassword'";
-                        $run = mysqli_query($conn, $sql);
-                        $fetch = mysqli_fetch_assoc($run);
-                        if(mysqli_num_rows($run) > 0)
-                        {
+                        $loginusername = $_REQUEST['loginusername'];
+                        $loginpassword = md5($_REQUEST['loginpassword']);
                         
-                                session_start();
-                                $_SESSION['id'] = $fetch['userID'];
-                                $_SESSION['username'] = $fetch['username'];
-                                //echo "<p style=\"color:red;\">".$_SESSION['id']."</p>";
-                               header('Location: profile.php');
-                         
+                        $collection = $db->user;
+
+                        $loginInfo = array("username"  => $username,
+                                "password" => $password,
+                                "email" => $email);
+                        $cursorFind = $collection->findOne($loginInfo);
+
+                        if (!empty($cursorFind)){
+                          session_start();
+                            $_SESSION['id'] = $fetch['userID'];
+                            $_SESSION['username'] = $fetch['username'];
+                            header('Location: profile.php');
                         }
-                        else 
+                         else 
                         {
                             $loginErr = "<p style=\"color:red;\">Username or password is incorrect</p>";
                         }
-                    // close connection
-                    mysqli_close($conn);
                     }
                 }
             ?>
