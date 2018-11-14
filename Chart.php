5a6b56d588663d698cb81652ffd0c64d42890dfc
QJunHao <jun_hao995@hotmail.com>
2018-11-14 22:50:20 +0800

    Add files via upload

--- Chart.php
+++ Chart.php
@@ -1,12 +1,13 @@
-<?php // include 'masterpage.php'; ?>
+<?php include 'masterpage.php'; ?>
 <!DOCTYPE HTML>
 <?php
    	include 'process/process_basicSetup.php';
    	date_default_timezone_set('Asia/Singapore');
-        $collection = $db->traffic_incident;
-
-
-    $cursor = $collection->find(array(), array("region" => 1,"message"=>1));
+    if (!$conn) {
+            die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
+    }
+    $sql = "SELECT region, message FROM Traffic_incident WHERE region <> 'None'";
+    $result = mysqli_query($conn, $sql);
     $regionArray = ["North", "North East", "West", "East", "Central"];
     $numPerRegion = [0, 0, 0, 0, 0];
     $past24HrArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -36,8 +37,9 @@
     		}
     	}	
     }
-	    foreach($cursor as $row){
-	    	if (((int)substr($row['Message'], 1, 2)) > 9){
+   	if($result){
+	    while($row=mysqli_fetch_row($result)){
+	    	if (((int)substr($row[1], 1, 2)) > 9){
 	    		$monthStringStart = 4;
 	    		$timeStringStart = 7;
 	    	}
@@ -45,7 +47,7 @@
 	    		$monthStringStart = 3;
 	    		$timeStringStart = 6;
     		}
-	    	if (((int)substr($row['Message'], $monthStringStart, 2)) > 9){
+	    	if (((int)substr($row[1], $monthStringStart, 2)) > 9){
 	    		$timeStringLength = 2;
 	    	}
 	    	else{
@@ -73,34 +75,34 @@
 	    			}
 	    		}
 	    		if ($startingDay == 28 || $startingDay == 30 || $startingDay == 31){
-	    			if ((int)substr($row['Message'], 1, 2) == $startingDay && (int)substr($row['Message'], $monthStringStart, $timeStringLength) == ($currentMonth - 1) && ((int)substr($row['Message'], $timeStringStart, $timeStringLength) >= $startingHr) && ((int)substr($row['Message'], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
+	    			if ((int)substr($row[1], 1, 2) == $startingDay && (int)substr($row[1], $monthStringStart, $timeStringLength) == ($currentMonth - 1) && ((int)substr($row[1], $timeStringStart, $timeStringLength) >= $startingHr) && ((int)substr($row[1], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
 		    			$past24HrArray[$j]++;
 			    		$totalIncident++;
 			    		
 			    		for ($i = 0; $i < sizeof($regionArray); $i++){
-			    			if ($row['region'] == $regionArray[$i]){
+			    			if ($row[0] == $regionArray[$i]){
 			    				$numPerRegion[$i]++;
 			    				break;
 			    			}
 			    		}
 		    		}
 	    		}
-	    		if ((int)substr($row['Message'], $monthStringStart, 2) == $currentMonth){
-	    			if (((int)substr($row['Message'], 1, 2) == $currentDate) && ((int)substr($row['Message'], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
+	    		if ((int)substr($row[1], $monthStringStart, 2) == $currentMonth){
+	    			if (((int)substr($row[1], 1, 2) == $currentDate) && ((int)substr($row[1], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
 	    				$past24HrArray[$j]++;
 		    			$totalIncident++;
 		    			for ($i = 0; $i < sizeof($regionArray); $i++){
-			    			if ($row['region'] == $regionArray[$i]){
+			    			if ($row[0] == $regionArray[$i]){
 			    				$numPerRegion[$i]++;
 			    				break;
     						}
     					}
 	    			}
-	    			else if (((int)substr($row['Message'], 1, 2) == $startingDay) && ((int)substr($row['Message'], $timeStringStart, $timeStringLength) >= $startingHr) && ((int)substr($row['Message'], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
+	    			else if (((int)substr($row[1], 1, 2) == $startingDay) && ((int)substr($row[1], $timeStringStart, $timeStringLength) >= $startingHr) && ((int)substr($row[1], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
 						$past24HrArray[$j]++;
 			    		$totalIncident++;
 			    		for ($i = 0; $i < sizeof($regionArray); $i++){
-			    			if ($row['region'] == $regionArray[$i]){
+			    			if ($row[0] == $regionArray[$i]){
 			    				$numPerRegion[$i]++;
 			    				break;
 			    			}
@@ -109,8 +111,8 @@
 	    		}
 	    	}
 	    }
+    }
     $averageIncident = $totalIncident/24;
-    
 ?>
 <html>
 <head>
@@ -125,31 +127,6 @@
 		var centralCount = <?php echo  $numPerRegion[4]; ?>;
 
 		// Second chart variable
-		var hour1 = <?php echo $past24HrTime[0]; ?>;
-		var hour2 = <?php echo $past24HrTime[1]; ?>;
-		var hour3 = <?php echo $past24HrTime[2]; ?>;
-		var hour4 = <?php echo $past24HrTime[3]; ?>;
-		var hour5 = <?php echo $past24HrTime[4]; ?>;
-		var hour6 = <?php echo $past24HrTime[5]; ?>;
-		var hour7 = <?php echo $past24HrTime[6]; ?>;
-		var hour8 = <?php echo $past24HrTime[7]; ?>;
-		var hour9 = <?php echo $past24HrTime[8]; ?>;
-		var hour10 = <?php echo $past24HrTime[9]; ?>;
-		var hour11 = <?php echo $past24HrTime[10]; ?>;
-		var hour12 = <?php echo $past24HrTime[11]; ?>;
-		var hour13 = <?php echo $past24HrTime[12]; ?>;
-		var hour14 = <?php echo $past24HrTime[13]; ?>;
-		var hour15 = <?php echo $past24HrTime[14]; ?>;
-		var hour16 = <?php echo $past24HrTime[15]; ?>;
-		var hour17 = <?php echo $past24HrTime[16]; ?>;
-		var hour18 = <?php echo $past24HrTime[17]; ?>;
-		var hour19 = <?php echo $past24HrTime[18]; ?>;
-		var hour20 = <?php echo $past24HrTime[19]; ?>;
-		var hour21 = <?php echo $past24HrTime[20]; ?>;
-		var hour22 = <?php echo $past24HrTime[21]; ?>;
-		var hour23 = <?php echo $past24HrTime[22]; ?>;
-		var hour24 = <?php echo $past24HrTime[23]; ?>;
-
 		var pastHrCount1 = <?php echo $past24HrArray[0]; ?>;
 		var pastHrCount2 = <?php echo $past24HrArray[1]; ?>;
 		var pastHrCount3 = <?php echo $past24HrArray[2]; ?>;
@@ -175,13 +152,15 @@
 		var pastHrCount23 = <?php echo $past24HrArray[22]; ?>;
 		var pastHrCount24 = <?php echo $past24HrArray[23]; ?>;
 
+		var averageIncident = <?php echo $averageIncident; ?>;
+
 		window.onload = function () {
 
 		var chart1 = new CanvasJS.Chart("chartContainer1", {
 			exportEnabled: true,
 			animationEnabled: true,
 			title:{
-				text: "Number of accidents from each region"
+				text: "Number of incidents per region"
 			},
 			legend:{
 				cursor: "pointer",
@@ -190,7 +169,7 @@
 			data: [{
 				type: "pie",
 				showInLegend: true,
-				toolTipContent: "{name}: <strong>{y} number of accidents</strong>",
+				toolTipContent: "{name}: <strong>{y} number of incidents</strong>",
 				indexLabel: "{name} - {y}",
 				dataPoints: [
 					{ y: northCount, name: "North"},
@@ -205,69 +184,59 @@
 
 		//Second chart
 		var chart2 = new CanvasJS.Chart("chartContainer2", {
-			animationEnabled: true,
-			zoomEnabled: true,
 			theme: "dark2",
+			animationEnabled: true,  
 			title:{
-				text: "Number of accidents for past 24 hours"
+				text: "Number of incidents for past 24 hours"
 			},
 			axisX:{
-		title: "Past 24 hours",
-		valueFormatString: "####",
-		interval: 2
-	},
-	
-		axisY:{
-			title: "Number of accidents",
-			titleFontColor: "#6D78AD",
-			logarithmic: false, //change it to false
-			lineColor: "#6D78AD",
-			gridThickness: 0,
-			lineThickness: 1,
-			labelFormatter: addSymbols
-	},
-		legend:{
-		verticalAlign: "top",
-		fontSize: 16,
-		dockInsidePlotArea: true
-	},
-		data: [{
-		type: "line",
-		xValueFormatString: "####",
-		showInLegend: true,
-		name: "Number of accidents",
+				title: "Past 24 hours",
+				valueFormatString: "####",
+				interval: 1
+			},
+			axisY: {
+				title: "Number of incidents",
+				valueFormatString: "####",
+				stripLines: [{
+					value: averageIncident,
+					label: "Average"
+				}]
+			},
+			data: [{
+				yValueFormatString: "####",
+				xValueFormatString: "####",
+				lineColor: "#6D78AD",
+				type: "spline",
 				dataPoints: [
-					{ x: hour1, y: pastHrCount1 },
-					{ x: hour2, y: pastHrCount2 },
-					{ x: hour3, y: pastHrCount3 },
-					{ x: hour4, y: pastHrCount4 },
-					{ x: hour5, y: pastHrCount5 },
-					{ x: hour6, y: pastHrCount6 },
-					{ x: hour7, y: pastHrCount7 },
-					{ x: hour8, y: pastHrCount8 },
-					{ x: hour9, y: pastHrCount9 },
-					{ x: hour10, y: pastHrCount10 },
-					{ x: hour11, y: pastHrCount11 },
-					{ x: hour12, y: pastHrCount12 },
-					{ x: hour13, y: pastHrCount13 },
-					{ x: hour14, y: pastHrCount14 },
-					{ x: hour15, y: pastHrCount15 },
-					{ x: hour16, y: pastHrCount16 },
-					{ x: hour17, y: pastHrCount17 },
-					{ x: hour18, y: pastHrCount18 },
-					{ x: hour19, y: pastHrCount19 },
-					{ x: hour20, y: pastHrCount20 },
-					{ x: hour21, y: pastHrCount21 },
-					{ x: hour22, y: pastHrCount22 },
-					{ x: hour23, y: pastHrCount23 }, 
-					{ x: hour24, y: pastHrCount24 }
-				]
-			}]
-			
-		});
-		chart2.render();
+							{ x: 1, y: pastHrCount1 },
+							{ x: 2, y: pastHrCount2 },
+							{ x: 3, y: pastHrCount3 },
+							{ x: 4, y: pastHrCount4 },
+							{ x: 5, y: pastHrCount5 },
+							{ x: 6, y: pastHrCount6 },
+							{ x: 7, y: pastHrCount7 },
+							{ x: 8, y: pastHrCount8 },
+							{ x: 9, y: pastHrCount9 },
+							{ x: 10, y: pastHrCount10 },
+							{ x: 11, y: pastHrCount11 },
+							{ x: 12, y: pastHrCount12 },
+							{ x: 13, y: pastHrCount13 },
+							{ x: 14, y: pastHrCount14 },
+							{ x: 15, y: pastHrCount15 },
+							{ x: 16, y: pastHrCount16 },
+							{ x: 17, y: pastHrCount17 },
+							{ x: 18, y: pastHrCount18 },
+							{ x: 19, y: pastHrCount19 },
+							{ x: 20, y: pastHrCount20 },
+							{ x: 21, y: pastHrCount21 },
+							{ x: 22, y: pastHrCount22 },
+							{ x: 23, y: pastHrCount23 },
+							{ x: 24, y: pastHrCount24 }
+						]
+					}]
+				});
+			chart2.render();
 		}
-
 		function explodePie (e) {
 			if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
 				e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
@@ -276,20 +245,13 @@
 			}
 			e.chart.render();
 		}
-		function addSymbols(e){
-			var suffixes = ["", "K", "M", "B"];
-
-			var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
-			if(order > suffixes.length - 1)
-				order = suffixes.length - 1;
-
-			var suffix = suffixes[order];
-			return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
-		}
 	</script>
 </head>
 <body>
-	<div id="chartContainer1" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 100px;"></div>
-	<div id="chartContainer2" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 150px;"></div>
+	<header>
+    	<h1 style="auto; margin-top: 80px; text-align: center;">Incident statistics for the past 24 hours</h1>
+  	</header>
+	<div id="chartContainer1" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 50px;"></div>
+	<div id="chartContainer2" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 50px;"></div>
 </body>
 </html>
