da89becb12aadd1af4c06b2f890c5fca9c6ee120
QJunHao <jun_hao995@hotmail.com>
2018-11-14 23:07:17 +0800

    Add files via upload

--- Chart.php
+++ Chart.php
@@ -1,130 +1,20 @@
-<?php include 'masterpage.php'; ?>
-<!DOCTYPE HTML>
-<?php
-   	include 'process/process_basicSetup.php';
-   	date_default_timezone_set('Asia/Singapore');
-    if (!$conn) {
-            die ('Failed to connect to MySQL: ' . mysqli_connect_error());	
-    }
-    $sql = "SELECT region, message FROM Traffic_incident WHERE region <> 'None'";
-    $result = mysqli_query($conn, $sql);
-    $regionArray = ["North", "North East", "West", "East", "Central"];
-    $numPerRegion = [0, 0, 0, 0, 0];
-    $past24HrArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    $totalIncident = 0;
-    $monthsWithPrevious31Days = [1, 2, 4, 6, 8, 9, 11];
-    $monthsWithPrevious30Days = [5, 7, 10, 12];
-    $currentMonth = date("m");
-    $currentDate = date("d");
-    $startingDay = date("d");
-    $startingHr =  date("H") - 23;
-
-    if ($startingHr < 0){
-    	$startingDay -= 1;
-    	$startingHr += 24;
-    }
-    if ($startingDay < 1){
-    	if ($currentMonth == 3)
-    		$startingDay = 28;
-    	else{
-    		for ($k = 0; $k < sizeof($monthsWithPrevious31Days); $k++){
-	    		if ($currentMonth == $monthsWithPrevious31Days[$k]){
-	    			$startingDay = 31;
-	    			break;
-	    		}
-    			else
-    				$startingDay = 30;
-    		}
-    	}	
-    }
-   	if($result){
-	    while($row=mysqli_fetch_row($result)){
-	    	if (((int)substr($row[1], 1, 2)) > 9){
-	    		$monthStringStart = 4;
-	    		$timeStringStart = 7;
-	    	}
-	    	else{
-	    		$monthStringStart = 3;
-	    		$timeStringStart = 6;
-    		}
-	    	if (((int)substr($row[1], $monthStringStart, 2)) > 9){
-	    		$timeStringLength = 2;
-	    	}
-	    	else{
-	    		$timeStringLength = 1;
-	    		$timeStringStart -= 1;
-	    	}
-	    	for ($j = 0; $j < sizeof($past24HrArray); $j++){
-	    		$currentHrIncrement = $startingHr + $j;
-	    		if ($currentHrIncrement > 23)
-	    			$currentHrIncrement -= 24;
-	    		if ($startingDay == 28 && $currentMonth == 3)
-	    			$currentDate = 1;
-	    		else{
-		    		for ($k = 0; $k < sizeof($monthsWithPrevious31Days); $k++){
-		    			if ($startingDay == 31 && $currentMonth == $monthsWithPrevious31Days[$k]){
-		    				$currentDate = 1;
-		    				break;
-	    				}
-	    			}	
-	    			for ($k = 0; $k < sizeof($monthsWithPrevious31Days); $k++){
-	    				if ($startingDay == 30 && $currentMonth == $monthsWithPrevious30Days[$k]){
-	    					$currentDate = 1;
-		    				break;
-		    			}
-	    			}
-	    		}
-	    		if ($startingDay == 28 || $startingDay == 30 || $startingDay == 31){
-	    			if ((int)substr($row[1], 1, 2) == $startingDay && (int)substr($row[1], $monthStringStart, $timeStringLength) == ($currentMonth - 1) && ((int)substr($row[1], $timeStringStart, $timeStringLength) >= $startingHr) && ((int)substr($row[1], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
-		    			$past24HrArray[$j]++;
-			    		$totalIncident++;
-			    		
-			    		for ($i = 0; $i < sizeof($regionArray); $i++){
-			    			if ($row[0] == $regionArray[$i]){
-			    				$numPerRegion[$i]++;
-			    				break;
-			    			}
-			    		}
-		    		}
-	    		}
-	    		if ((int)substr($row[1], $monthStringStart, 2) == $currentMonth){
-	    			if (((int)substr($row[1], 1, 2) == $currentDate) && ((int)substr($row[1], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
-	    				$past24HrArray[$j]++;
-		    			$totalIncident++;
-		    			for ($i = 0; $i < sizeof($regionArray); $i++){
-			    			if ($row[0] == $regionArray[$i]){
-			    				$numPerRegion[$i]++;
-			    				break;
-    						}
-    					}
-	    			}
-	    			else if (((int)substr($row[1], 1, 2) == $startingDay) && ((int)substr($row[1], $timeStringStart, $timeStringLength) >= $startingHr) && ((int)substr($row[1], $timeStringStart, $timeStringLength) == $currentHrIncrement)){
-						$past24HrArray[$j]++;
-			    		$totalIncident++;
-			    		for ($i = 0; $i < sizeof($regionArray); $i++){
-			    			if ($row[0] == $regionArray[$i]){
-			    				$numPerRegion[$i]++;
-			    				break;
-			    			}
-			    		}
-			    	}
-	    		}
-	    	}
-	    }
-    }
-    $averageIncident = $totalIncident/24;
+<?php 
+	include 'masterpage.php'; 
+	include 'process/process_basicSetup.php';
+	include 'process/process_chart.php';
 ?>
+<!DOCTYPE HTML>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<script src="chartjs/canvasjs.min.js"></script>
 	<script>
 		// First chart variable
-		var northCount = <?php echo  $numPerRegion[0]; ?>;
-		var northEastCount = <?php echo  $numPerRegion[1]; ?>;
-		var westCount = <?php echo  $numPerRegion[2]; ?>;
-		var eastCount = <?php echo  $numPerRegion[3]; ?>;
-		var centralCount = <?php echo  $numPerRegion[4]; ?>;
+		var northCount = <?php echo $numPerRegion[0]; ?>;
+		var northEastCount = <?php echo $numPerRegion[1]; ?>;
+		var westCount = <?php echo $numPerRegion[2]; ?>;
+		var eastCount = <?php echo $numPerRegion[3]; ?>;
+		var centralCount = <?php echo $numPerRegion[4]; ?>;
 
 		// Second chart variable
 		var pastHrCount1 = <?php echo $past24HrArray[0]; ?>;
@@ -160,7 +50,7 @@
 			exportEnabled: true,
 			animationEnabled: true,
 			title:{
-				text: "Number of incidents per region"
+				text: "Number of accidents from each region"
 			},
 			legend:{
 				cursor: "pointer",
@@ -169,7 +59,7 @@
 			data: [{
 				type: "pie",
 				showInLegend: true,
-				toolTipContent: "{name}: <strong>{y} number of incidents</strong>",
+				toolTipContent: "{name}: <strong>{y} number of accidents</strong>",
 				indexLabel: "{name} - {y}",
 				dataPoints: [
 					{ y: northCount, name: "North"},
@@ -208,35 +98,36 @@
 				lineColor: "#6D78AD",
 				type: "spline",
 				dataPoints: [
-							{ x: 1, y: pastHrCount1 },
-							{ x: 2, y: pastHrCount2 },
-							{ x: 3, y: pastHrCount3 },
-							{ x: 4, y: pastHrCount4 },
-							{ x: 5, y: pastHrCount5 },
-							{ x: 6, y: pastHrCount6 },
-							{ x: 7, y: pastHrCount7 },
-							{ x: 8, y: pastHrCount8 },
-							{ x: 9, y: pastHrCount9 },
-							{ x: 10, y: pastHrCount10 },
-							{ x: 11, y: pastHrCount11 },
-							{ x: 12, y: pastHrCount12 },
-							{ x: 13, y: pastHrCount13 },
-							{ x: 14, y: pastHrCount14 },
-							{ x: 15, y: pastHrCount15 },
-							{ x: 16, y: pastHrCount16 },
-							{ x: 17, y: pastHrCount17 },
-							{ x: 18, y: pastHrCount18 },
-							{ x: 19, y: pastHrCount19 },
-							{ x: 20, y: pastHrCount20 },
-							{ x: 21, y: pastHrCount21 },
-							{ x: 22, y: pastHrCount22 },
-							{ x: 23, y: pastHrCount23 },
-							{ x: 24, y: pastHrCount24 }
-						]
-					}]
-				});
-			chart2.render();
+					{ x: 1, y: pastHrCount1 },
+					{ x: 2, y: pastHrCount2 },
+					{ x: 3, y: pastHrCount3 },
+					{ x: 4, y: pastHrCount4 },
+					{ x: 5, y: pastHrCount5 },
+					{ x: 6, y: pastHrCount6 },
+					{ x: 7, y: pastHrCount7 },
+					{ x: 8, y: pastHrCount8 },
+					{ x: 9, y: pastHrCount9 },
+					{ x: 10, y: pastHrCount10 },
+					{ x: 11, y: pastHrCount11 },
+					{ x: 12, y: pastHrCount12 },
+					{ x: 13, y: pastHrCount13 },
+					{ x: 14, y: pastHrCount14 },
+					{ x: 15, y: pastHrCount15 },
+					{ x: 16, y: pastHrCount16 },
+					{ x: 17, y: pastHrCount17 },
+					{ x: 18, y: pastHrCount18 },
+					{ x: 19, y: pastHrCount19 },
+					{ x: 20, y: pastHrCount20 },
+					{ x: 21, y: pastHrCount21 },
+					{ x: 22, y: pastHrCount22 },
+					{ x: 23, y: pastHrCount23 }, 
+					{ x: 24, y: pastHrCount24 }
+				]
+			}]
+		});
+		chart2.render();
 		}
+
 		function explodePie (e) {
 			if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
 				e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
@@ -245,13 +136,20 @@
 			}
 			e.chart.render();
 		}
+		function addSymbols(e){
+			var suffixes = ["", "K", "M", "B"];
+
+			var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
+			if(order > suffixes.length - 1)
+				order = suffixes.length - 1;
+
+			var suffix = suffixes[order];
+			return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+		}
 	</script>
 </head>
 <body>
-	<header>
-    	<h1 style="auto; margin-top: 80px; text-align: center;">Incident statistics for the past 24 hours</h1>
-  	</header>
-	<div id="chartContainer1" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 50px;"></div>
-	<div id="chartContainer2" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 50px;"></div>
+	<div id="chartContainer1" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 100px;"></div>
+	<div id="chartContainer2" style="height: 370px; max-width: 920px; margin: 0px auto; margin-top: 150px;"></div>
 </body>
 </html>
