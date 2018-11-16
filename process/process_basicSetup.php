45602e48d429a7af8cdc93b7b41dabc2be1ae473
Desmondtjc <43051779+Desmondtjc@users.noreply.github.com>
2018-11-16 21:42:15 +0800

    alicloud for mongodb connection done

--- process/process_basicSetup.php
+++ process/process_basicSetup.php
@@ -2,7 +2,7 @@
 
    require '..\..\htdocs\ICT2103MDB\vendor\autoload.php';
    
-   $connect = new MongoDB\Client();
+   $connect = new MongoDB\Client("mongodb://root:SITcloud9@dds-gs5cbb2e1fbc71c41684-pub.mongodb.singapore.rds.aliyuncs.com:3717,dds-gs5cbb2e1fbc71c42879-pub.mongodb.singapore.rds.aliyuncs.com:3717/admin?replicaSet=mgset-300213086&authSource=admin");
    $db = $connect ->ict2103;
 //   $collection = $db->user;
 //   $cursor = $collection->find();
